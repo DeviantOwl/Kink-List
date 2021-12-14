76eb6f7e838e13ab9fba24a1cb05be6d89ab8859
TempusOwl <Olympus@binarychronos.com>
2021-12-14 01:24:16 -0500

    Clean Up Formatting

--- js/selections.js
+++ js/selections.js
@@ -1,35 +1,36 @@
-
-  $(document).ready(function(){
+$(document).ready(function () {
     //state default radio ':checked' property there:
     $("input:radio:first").prop("checked", true);
 
-    $("#self-biting input:radio").click(function() {
+    $("#self-biting input:radio").click(function () {
         alert("clicked");
     });
-    $("#partner-biting input:radio").click(function() {
+    $("#partner-biting input:radio").click(function () {
         alert("clicked");
     });
-    $("#radio3 input:radio").click(function() {
+    $("#radio3 input:radio").click(function () {
         alert("clicked");
     });
 
     // overwrite radio ':checked' property there (if exists in localStorage) :
-    $('input[type=radio]').each(function(){
-        var state = JSON.parse( localStorage.getItem('r_'  + this.id) );
+    $('input[type=radio]').each(function () {
+        var state = JSON.parse(localStorage.getItem('r_' + this.id));
 
         if (state) this.checked = state.checked;
     });
 });
 
 var ele = document.getElementsByName("Choose");
-   for(var i=0;i<ele.length;i++)
-      ele[i].checked = false;
+for (var i = 0; i < ele.length; i++)
+    ele[i].checked = false;
 
 
-$(window).bind('unload', function(){
-    $('input[type=radio]').each(function(){
+$(window).bind('unload', function () {
+    $('input[type=radio]').each(function () {
         localStorage.setItem(
-            'r_' + this.id, JSON.stringify({checked: this.checked})
+            'r_' + this.id, JSON.stringify({
+                checked: this.checked
+            })
         );
     });
 });
